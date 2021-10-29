d04b7113e29952d7d377b860e0bc75d652779a64
Falarad <pimentel611@gmail.com>
2021-10-28 22:34:04 -0400

    Update Portfolio.php

--- Portfolio.php
+++ Portfolio.php
@@ -21,7 +21,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="www.migueljpimentel.com#achievements">Achievements</a>
+                            <a class="nav-link" href="..#achievements">Achievements</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="www.migueljpimentel.com#skills">Skills</a>
